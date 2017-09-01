9736ebd4ba38970a03b5fd0e4ee2fab7bc4dd0e8
Vadim Pavlyuk <pavlyuk.dev@gmail.com>
2017-09-01 10:45:06 +0300

    initial component 'Filter'

--- app/components/App.js
+++ app/components/App.js
@@ -1,10 +1,24 @@
 import {h, Component} from 'preact';
 
+import Header from './Header';
+import Filters from './Filters';
+import Card from './Card';
+
 export default class App extends Component {
   render() {
     return (
-      <div id="content">
-        <h5>Time to <a href="https://github.com/developit/preact/">Preact</a>.</h5>
+      <div>
+        <Header />
+        <div class="container">
+          <div class="row">
+            <div class="col-md-9">
+              <Card />
+            </div>
+            <div class="col-md-3">
+              <Filters />    
+            </div>
+          </div> 
+        </div>
       </div>
     );
   }
---
+++ app/components/Filters/index.js
@@ -0,0 +1,14 @@
+import {h, Component} from 'preact';
+
+class Filters extends Component {
+  
+  render() {
+    return (
+      <div class="Filters">
+        Filters
+      </div>
+    );
+  }
+}
+
+export default Filters;
