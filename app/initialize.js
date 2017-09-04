0355f5b5e08241e72a7d67feff34230e0c89547d
Vadim Pavlyuk <pavlyuk.dev@gmail.com>
2017-09-05 01:46:27 +0300

    routing test

--- app/initialize.js
+++ app/initialize.js
@@ -2,16 +2,19 @@ import {h, render} from 'preact';
 import Router, { route } from 'preact-router';
 import Main from './containers/Main';
 
-
 document.addEventListener('DOMContentLoaded', () => {
   const app = document.getElementById('app');
   while (app.firstChild) {
     app.removeChild(app.firstChild);
   }
+
+  process.env.PUBLIC_URL = process.env.PUBLIC_URL || '/app.github.io';
   const App = () => (
-    <Router hashHistory={true}>
-      <Main route={route}/>
+    <Router>
+      <Main path='/' route={route}/>
+      <Main path='/app.github.io' route={route}/>
     </Router>
+
   );
   render(<App />, app);
 });
