f8cb1916221aca8b63dfb2ebe55ca154f72c6059
Vadim Pavlyuk <pavlyuk.dev@gmail.com>
2017-09-09 23:07:54 +0300

    add component Sidebar

---
+++ app/components/SideBar/index.js
@@ -0,0 +1,55 @@
+import {h, Component} from 'preact';
+import Filters from '../Filters';
+import Sorting from '../Sorting';
+
+class SideBar extends Component {
+  constructor(props) {
+    super(props);
+    this.changeHasTopics = this.changeHasTopics.bind(this);
+    this.changeHasOpenIssues = this.changeHasOpenIssues.bind(this);
+  }
+    
+  changeHasTopics(hasTopics) {
+    const newValue = this.props.filtersParams.hasTopics === true ? false : true;
+    this.props.changeHasTopics(newValue);
+  }
+  
+  changeHasOpenIssues(hasOpenIssues) {
+    const newValue = this.props.filtersParams.hasOpenIssues === true ? false : true;
+    this.props.changeHasOpenIssues(newValue);
+  }
+
+  render({
+    sortingParams,
+    changeSortBy,
+    changeSortOrder,
+    languages,
+    filtersParams,
+    changeLanguage,
+    changeType,
+    changeNumberOfStars,
+    changeUpdatedDate
+  }) {
+    return (
+      <div>
+        <Sorting
+          sortingParams={sortingParams}
+          changeSortBy={changeSortBy}
+          changeSortOrder={changeSortOrder}
+        />
+        <Filters
+          languages={languages}
+          filtersParams={filtersParams}
+          changeLanguage={changeLanguage}
+          changeType={changeType}
+          changeNumberOfStars={changeNumberOfStars}
+          changeUpdatedDate={changeUpdatedDate}
+          changeHasTopics={this.changeHasTopics}
+          changeHasOpenIssues={this.changeHasOpenIssues}
+        />
+      </div>
+    );
+  }
+}
+
+export default SideBar;
