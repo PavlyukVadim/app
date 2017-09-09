7f896dee61b2c6a04a0766b9093cbaac8c56a85d
Vadim Pavlyuk <pavlyuk.dev@gmail.com>
2017-09-09 23:12:29 +0300

    change ControlBar to Redux container

--- app/containers/ControlBar/index.js
+++ app/containers/ControlBar/index.js
@@ -1,86 +1,38 @@
 import {h, Component} from 'preact';
-import Filters from '../../components/Filters';
-import Sorting from '../../components/Sorting';
+import { connect } from 'preact-redux';
+import { changeFilters, changeSorting } from '../../actions';
+import SideBar from '../../components/SideBar';
 
-class ControlBar extends Component {
-  constructor(props) {
-    super(props);
-    this.changeLanguage = this.changeLanguage.bind(this);
-    this.changeType = this.changeType.bind(this);
-    this.changeNumberOfStars = this.changeNumberOfStars.bind(this);
-    this.changeUpdatedDate = this.changeUpdatedDate.bind(this);
-    this.changeHasTopics = this.changeHasTopics.bind(this);
-    this.changeHasOpenIssues = this.changeHasOpenIssues.bind(this);
-  }
 
-  componentWillReceiveProps(nextProps) {
-    this.setState({
-      languages: this.getLanguages(nextProps.repos),
-    });
-  }
+const getLanguages = (repos = []) => {
+  let languages = [];
+  repos.forEach((repo) => {
+    if(!~languages.indexOf(repo.language) && repo.language) {
+      languages.push(repo.language);
+    }
+  });
+  return languages;
+}
 
-  getLanguages(repos) {
-    let languages = [];
-    repos.forEach((repo) => {
-      if(!~languages.indexOf(repo.language) && repo.language) {
-        languages.push(repo.language);
-      }
-    });
-    return languages;
+const mapStateToProps = (state) => {
+  return {
+    languages: getLanguages(state.repos),
+    sortingParams: state.sortingParams,
+    filtersParams: state.filtersParams,
   }
-
-  changeLanguage(language) {
-    this.props.filtersParamsOnChange({language});
-  }
+}
   
-  changeType(type) {
-    this.props.filtersParamsOnChange({type});
-  }
-
-  changeNumberOfStars(numberOfStars) {
-    this.props.filtersParamsOnChange({numberOfStars});
-  }
-
-  changeUpdatedDate(updatedDate) {
-    this.props.filtersParamsOnChange({updatedDate});
-  }
-  
-  changeHasTopics(hasTopics) {
-    const newValue = this.props.filtersParams.hasTopics === true ? false : true;
-    this.props.filtersParamsOnChange({
-      hasTopics: newValue,
-    });
-  }
-  
-  changeHasOpenIssues(hasOpenIssues) {
-    const newValue = this.props.filtersParams.hasOpenIssues === true ? false : true;
-    this.props.filtersParamsOnChange({
-      hasOpenIssues: newValue,
-    });
-  }
-
-  render({ sortOnChange, sortOrderOnChange, sortBy, sortOrder, filtersParams }, { languages }) {
-    return (
-      <div>
-        <Sorting
-          sortBy={sortBy}
-          sortOrder={sortOrder}
-          sortOnChange={sortOnChange}
-          sortOrderOnChange={sortOrderOnChange}
-        />
-        <Filters
-          languages={languages}
-          filtersParams={filtersParams}
-          changeLanguage={this.changeLanguage}
-          changeType={this.changeType}
-          changeNumberOfStars={this.changeNumberOfStars}
-          changeUpdatedDate={this.changeUpdatedDate}
-          changeHasTopics={this.changeHasTopics}
-          changeHasOpenIssues={this.changeHasOpenIssues}
-        />
-      </div>
-    );
+const mapDispatchToProps = (dispatch) => {
+  return {
+    changeSortBy: (sortBy) => dispatch(changeSorting({sortBy})),
+    changeSortOrder: (sortOrder) => dispatch(changeSorting({sortOrder})),
+    changeLanguage: (language) => dispatch(changeFilters({language})),
+    changeType: (type) => dispatch(changeFilters({type})),
+    changeNumberOfStars: (numberOfStars) => dispatch(changeFilters({numberOfStars})),    
+    changeUpdatedDate: (updatedDate) => dispatch(changeFilters({updatedDate})),
+    changeHasTopics: (hasTopics) => dispatch(changeFilters({hasTopics})),
+    changeHasOpenIssues: (hasOpenIssues) => dispatch(changeFilters({hasOpenIssues})),
   }
 }
 
-export default ControlBar;
+export default connect(mapStateToProps, mapDispatchToProps)(SideBar);
