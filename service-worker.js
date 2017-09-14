22e13eabc22f97f855e35d4b9aeb5d0ec2362aff
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2017-09-14 20:38:19 +0000

    Deploy PavlyukVadim/app.github.io to github.com/PavlyukVadim/app.github.io.git:master

---
+++ service-worker.js
@@ -0,0 +1,46 @@
+// Set this to true for production
+var doCache = true;
+// var doCache = false;
+
+// Name our cache
+var CACHE_NAME = 'my-pwa-cache-v1';
+ 
+// Delete old caches that are not our current one!
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys()
+      .then(keyList =>
+        Promise.all(keyList.map(key => {
+          if (!cacheWhitelist.includes(key)) {
+            console.log('Deleting cache: ' + key)
+            return caches.delete(key);
+          }
+        }))
+      )
+  );
+});
+ 
+// The first time the user starts up the PWA, 'install' is triggered.
+self.addEventListener('install', function(event) {
+  if (doCache) {
+    event.waitUntil(
+      caches.open(CACHE_NAME)
+        .then(function(cache) {
+          const urlsToCache = ['/'];
+          cache.addAll(urlsToCache)
+          console.log('cached');
+        })
+    );
+  }
+});
+ 
+self.addEventListener('fetch', function(event) {
+  if (doCache) {
+    event.respondWith(
+      caches.match(event.request).then(function(response) {
+        return response || fetch(event.request);
+      })
+    );
+  }
+});
